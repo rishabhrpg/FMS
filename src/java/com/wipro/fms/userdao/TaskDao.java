a352e7c67fad24c87f093926e08da46f1a32b589
Yogi <Yogi@DESKTOP-NIVAJLC>
2017-06-02 19:18:03 +0530

    add task added

---
+++ src/java/com/wipro/fms/userdao/TaskDao.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wipro.fms.userdao;
+
+import com.wipro.fms.beans.TaskBean;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author Yogi
+ */
+public class TaskDao {
+
+    public static boolean addTask(TaskBean task) {
+         try{
+            Connection conn = DBHelper.getDbConnection();
+            
+            PreparedStatement pst = conn.prepareStatement("insert into tasks(name,stime,etime) values(?,?,?)");
+            pst.setString(1,task.getStime());
+            pst.setString(2,task.getEtime());
+            pst.setString(3,task.getName());
+             if(pst.execute())
+             {
+                 return true;
+             }
+           else{
+                return false;
+            }
+        }catch(Exception x){
+            System.out.println("Error : "+x);
+            return false;
+        }
+        
+    }
+    
+}
