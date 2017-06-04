5233833c4859df13aa5495737a765c1c5a160456
Yogi <Yogi@DESKTOP-NIVAJLC>
2017-06-05 01:38:18 +0530

    updeted entried in web.xml

---
+++ src/java/com/wipro/fms/servelets/ProUpdateTrainer.java
@@ -0,0 +1,198 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wipro.fms.servelets;
+
+import com.wipro.fms.beans.UsersBean;
+import com.wipro.fms.userdao.TrainerDao;
+import com.wipro.fms.userdao.UserDao;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Yogi
+ */
+public class ProUpdateTrainer extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException, ParseException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+             HttpSession session = request.getSession();
+            String fname=request.getParameter("firstname");
+            String lname = request.getParameter("lastname");
+            String username=request.getParameter("username");            
+            String password = request.getParameter("password");
+            String spec = request.getParameter("spec");
+            //out.print(request.getParameter("dob"));
+           
+           java.sql.Date dob = java.sql.Date.valueOf(request.getParameter("dob"));
+           
+            String email = request.getParameter("email");
+            String contactno = request.getParameter("contactno");
+            String address = request.getParameter("address");
+            java.sql.Date doj=null;
+             UsersBean task = new UsersBean(fname,lname,dob,doj,contactno, email,address,username,password,"trainer",spec);
+             if(TrainerDao.updateTrainer(task))
+             {
+                 System.out.println("task addeded successfully");
+                  request.getRequestDispatcher("index.head.html").include(request, response);
+               request.getRequestDispatcher("welcome.nav.html").include(request, response);
+                out.println("<br><div class='container-fluid'>");
+               out.println("<div class='row'>"); 
+               out.println("<div class='col-md-3 col-sm-4 animated fadeIn'>");
+               out.println("<div class=\"\">\n" +
+                "<div class=\"w3-card-4 test\" style=\"color:#ffffff;background-color:#0088cc;width:92%;\">\n" +
+                "  <img src=\"img_avatar3.png\" alt=\"Avatar\" style=\"width:100%;opacity:0.85\">\n" +
+                "  <div class=\"w3-container \" >\n" +
+                "  <h4><b>"+UserDao.getUserData(session,"firstname")+" "+UserDao.getUserData(session,"lastname")+"</b></h4>    \n");
+                out.println("<div class='row'>");
+                    out.println("<div class='col-xs-6'>");
+                        out.println("Username : ");
+                    out.println("</div>");
+                    out.println("<div class='col-xs-6'>");
+                        out.println(UserDao.getUserData(session,"username"));
+                    out.println("</div>");
+                out.println("</div>");
+                out.println("<div class='row'>"); 
+                    out.println("<div class='col-xs-6'>");
+                        out.println("Date of Joining: ");
+                    out.println("</div>");
+                    out.println("<div class='col-xs-6'>");
+                        out.println(UserDao.getUserData(session,"doj"));
+                    out.println("</div>");
+                out.println("</div>");
+                out.println("<div class='row'>");
+                    out.println("<div class='col-xs-6'>");
+                        out.println("Date of Birth: ");
+                    out.println("</div>");
+                    out.println("<div class='col-xs-6'>");
+                        out.println(UserDao.getUserData(session,"dob"));
+                    out.println("</div>");
+                out.println("</div>");
+                out.println("<div class='row'>");
+                    out.println("<div class='col-xs-6'>");
+                        out.println("Contact No: ");
+                    out.println("</div>");
+                    out.println("<div class='col-xs-6'>");
+                        out.println(UserDao.getUserData(session,"contact_no"));
+                    out.println("</div>");
+                out.println("</div>");
+                out.println("<div class='row'>");
+                    out.println("<div class='col-xs-6'>");
+                        out.println("Email : ");
+                    out.println("</div>");
+                    out.println("<div class='col-xs-6'>");
+                        out.println(UserDao.getUserData(session,"email"));
+                    out.println("</div>");
+                out.println("</div>");
+                out.println("<div class='row'>");
+                    out.println("<div class='col-xs-6'>");
+                        out.println("Address: ");
+                    out.println("</div>");
+                    out.println("<div class='col-xs-6'>");
+                        out.println(UserDao.getUserData(session,"address"));
+                    out.println("</div>");
+                out.println("</div>");
+                out.println("<div class='row'>");
+                    out.println("<div class='col-xs-6'>");
+                        out.println("Account Type: ");
+                    out.println("</div>");
+                    out.println("<div class='col-xs-6'>");
+                        out.println(UserDao.getUserData(session,"role"));
+                    out.println("</div>");
+                out.println("</div>");
+                
+                out.println("<div class='clear-fix'><br></div>");
+                out.println("  </div>\n" +
+                "</div>\n" +
+                "<br>\n" +
+                "</div>");
+               out.println("</div>");
+               out.println("<div class='col-md-4 col-sm-8  animated fadeInDown'>");
+               out.print("<div class=\"w3-panel w3-green  w3-card w3-round-xxlarge\">\n" +
+                "  <h3>Success!</h3>\n" +
+                "  <p>Trainer "+task.getFirstname()+" "+task.getLastname()+" is added successfully.</p>\n" +
+                "</div> ");
+               request.getRequestDispatcher("index.footer.html").include(request, response);                 
+             }
+             else
+             {
+                 System.out.println("task addition failed");
+             }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProUpdateTrainer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParseException ex) {
+            Logger.getLogger(ProUpdateTrainer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProUpdateTrainer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParseException ex) {
+            Logger.getLogger(ProUpdateTrainer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
